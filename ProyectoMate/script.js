5b7ab497b6de1e7a8055433f07cb7347843e2a50
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2025-05-28 10:09:48 -0300

    programa de mates

--- ProyectoMate/script.js
+++ ProyectoMate/script.js
@@ -1,49 +1,80 @@
 let form = document.getElementById("formulario")
-let boton1 = document.getElementById("subir")
 let input_inicio = document.getElementById("inicio")
 let input_final = document.getElementById("final")
 let divisiones = document.getElementById("puntos")
-let funcion = document.getElementById("selector").value
+let select = document.getElementById("selector")
 let pi = Math.PI
-let eje_x
-boton1.addEventListener("submit", () => {
-    inicio = input_inicio.value
-    final = input_final.value
+let funcionelegida = "sin"
+let eje_x = []
+console.log(eje_x)
+
+
+
+
+let text_cant_pt = document.getElementById("dots");
+divisiones.addEventListener("input", () => {
+    cantidad_puntos = parseInt(divisiones.value);
+    text_cant_pt.textContent = `${cantidad_puntos}`;
+});
+
+let text_cant_a = document.getElementById("cant");
+let cant_a = document.getElementById("a")
+cant_a.addEventListener("input", () => {
+    cantidad_a = parseInt(cant_a.value);
+    text_cant_a.textContent = `${cantidad_a}`;
+});
+
+function miFuncion1(x, tipo){
+    if(tipo === "sin"){
+        funcionelegida = "sin"
+        return cantidad_a*(Math.sin(x))
+    } else if(tipo === "cos"){
+        funcionelegida = "cos"
+        return cantidad_a*(Math.cos(x))
+    } else if(tipo === "tg"){
+        funcionelegida = "tan"
+        return cantidad_a*(Math.tan(x))
+    }
+}
+form.addEventListener("submit", (e) => {
+    
+    e.preventDefault()
+    //agarro los input
+    let puntos = parseInt(divisiones.value)
+    let inicio = parseFloat(parseFloat(input_inicio.value)*pi.toFixed(2))
+    let final = parseFloat(parseFloat(input_final.value)*pi.toFixed(2))
+
+    //Cada cuanto va un punto
+    let valor = (final-inicio)/(puntos-1)
+
+    //Genero los puntos
     eje_x = [inicio]
-    console.log(eje_x)
-    for(let i=1;i>=divisiones;i++){
-        let punto = i*(inicio-final/divisiones-1)
+    for(let i=1;i<puntos;i++){
+        let punto = i*valor
+        punto = parseFloat(punto.toFixed(2))
         eje_x.push(punto)
     }
-    eje_x.push(final)
+
     console.log(eje_x)
     form.remove()
+    
+    let grafica = `f(x)=${cantidad_a}${funcionelegida}(x)`
+    //Graficar funcion
+    const data = {
+    type: 'line',
+    data: {
+        labels: eje_x ,
+        datasets: [{
+        label: `${grafica}`,
+        data:  eje_x.map(x => miFuncion1(x,select.value)),
+        fill: true,
+        borderColor: 'rgb(9, 43, 192)',
+        borderWidth: 2,
+        tension: 0.2
+        }
+    ]
+    }
+};
+const canva = document.getElementById("grafico")
+window.miFuncion1 = new Chart(canva,data)
 })
-console.log(eje_x)
-function miFuncion1(x){
-    return Math.funcion(x)
-}
-// const data = {
-//     type: 'line',
-//     data: {
-//         labels: eje_x ,
-//         datasets: [{
-//         label: 'Coseno',
-//         data:  eje_x.map(x => miFuncion1(x)),
-//         fill: true,
-//         borderColor: 'rgb(9, 43, 192)',
-//         borderWidth: 2,
-//         tension: 0.2
-//         },{
-//             label: 'Seno',
-//             data:  eje_x.map(x => miFuncion2(x)),
-//             fill: true,
-//             borderColor: 'rgb(230, 6, 6)',
-//             borderWidth: 2,
-//             tension: 0.2  
-//         }
-//     ]
-//     }
-// };
-// const canva = document.getElementById("grafico")
-// window.miFuncion1 = new Chart(canva,data)
